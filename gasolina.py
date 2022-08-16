73d0a4484e32d7ee86774004769685d840150eb2
luedw <luisaeduardapiovesan@gmail.com>
2022-08-16 21:24:47 +0000

    arquivo gasolina.py alterado

--- gasolina.py
+++ gasolina.py
@@ -1,1 +1,9 @@
-print("Hello, EBAC!")
+import csv
+
+with open('gasolina.csv') as file:
+  line_reader = csv.reader(file)
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+gasolina = pd.read_csv('gasolina.csv',sep=',')
